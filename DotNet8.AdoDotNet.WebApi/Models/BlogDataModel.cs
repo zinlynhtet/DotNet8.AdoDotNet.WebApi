b5c8f55ecdcfd6311e4964c551a1b36bf02da904
Zin Linn Htet <mackk5504@gmail.com>
2024-05-06 22:38:58 +0630

    Complete Data Model

---
+++ DotNet8.AdoDotNet.WebApi/Models/BlogDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotNetTrainingBatch4.RestApi.Models;
+
+[Table("Tbl_Blogs")]
+public class BlogDataModel
+{
+    [Key]
+    public int BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+}
