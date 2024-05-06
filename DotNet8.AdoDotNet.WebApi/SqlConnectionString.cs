273907205b980e11979d552867b23a3fd5193a96
Zin Linn Htet <mackk5504@gmail.com>
2024-05-06 22:39:23 +0630

    Complete Connection String

---
+++ DotNet8.AdoDotNet.WebApi/SqlConnectionString.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace DotNet8.AdoDotNet.WebApi;
+
+internal static class SqlConnectionString
+{
+    public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+    {
+        DataSource = ".",
+        InitialCatalog = "TestDb",
+        UserID = "sa",
+        Password = "sasa@123",
+        TrustServerCertificate = true
+    };
+}
