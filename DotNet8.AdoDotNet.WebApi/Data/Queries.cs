e0d008b979c80fd37447ddbe1ebba138aa330a4d
Zin Linn Htet <mackk5504@gmail.com>
2024-05-07 14:55:10 +0630

    Complete Data Access

--- DotNet8.AdoDotNet.WebApi/Data/Queries.cs
+++ DotNet8.AdoDotNet.WebApi/Data/Queries.cs
@@ -20,4 +20,41 @@ public static class Queries
      WHERE BlogId = @BlogId";
     public static string DeleteQuery { get; } = @"DELETE FROM [dbo].[Tbl_Blogs]
     WHERE BlogId = @BlogId";
+    
+    public static string PatchQuery(string conditions)
+    {
+        string query = $@"UPDATE [dbo].[Tbl_Blog] SET {conditions} WHERE BlogId = @BlogId";
+        return query;
+    }
+    public static string Conditions(int id, BlogDataModel reqModel, List<AdoDotNetParameter> parameters)
+    {
+        var conditions = string.Empty;
+        if (!string.IsNullOrEmpty(reqModel.BlogTitle))
+        {
+            conditions += " [BlogTitle] = @BlogTitle, ";
+            parameters.Add(new AdoDotNetParameter("@BlogTitle", reqModel.BlogTitle));
+        }
+
+        if (!string.IsNullOrEmpty(reqModel.BlogAuthor))
+        {
+            conditions += " [BlogAuthor] = @BlogAuthor, ";
+            parameters.Add(new AdoDotNetParameter("@BlogAuthor", reqModel.BlogAuthor));
+        }
+
+        if (!string.IsNullOrEmpty(reqModel.BlogContent))
+        {
+            conditions += " [BlogContent] = @BlogContent, ";
+            parameters.Add(new AdoDotNetParameter("@BlogContent", reqModel.BlogContent));
+        }
+
+        if (conditions.Length == 0)
+        {
+            throw new Exception("Invalid conditions");
+        }
+
+        parameters.Add(new AdoDotNetParameter("@BlogId", id));
+        conditions = conditions.Substring(0, conditions.Length - 2);
+        return conditions;
+    }
+
 }
