3f69a3c734c7937a53f1220a665d636332dc0329
Zin Linn Htet <mackk5504@gmail.com>
2024-05-07 14:17:21 +0630

    Add Parameter Class

---
+++ DotNet8.AdoDotNet.Shared/AdoDotNetParameter.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNet8.AdoDotNet.Shared;
+
+public  class AdoDotNetParameter
+{
+    public AdoDotNetParameter()
+    {
+    }
+
+    public AdoDotNetParameter(string name, object value)
+    {
+        Name = name;
+        Value = value;
+    }
+
+    public string Name { get; set; }
+    public object Value { get; set; }
+}
