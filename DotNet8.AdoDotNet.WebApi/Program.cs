95eebf2254ec4ae6f4ef2504849d10d4af7f608f
Zin Linn Htet <mackk5504@gmail.com>
2024-05-07 14:18:13 +0630

    Add Service

--- DotNet8.AdoDotNet.WebApi/Program.cs
+++ DotNet8.AdoDotNet.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using DotNet8.AdoDotNet.WebApi.Features;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<Application>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
