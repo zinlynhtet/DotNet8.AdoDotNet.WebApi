0f378498b99aaf5e528db3eb71668927c28b07a6
Zin Linn Htet <mackk5504@gmail.com>
2024-05-07 14:17:59 +0630

    Complete interface

---
+++ DotNet8.AdoDotNet.WebApi/Features/IApplication.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNet8.AdoDotNet.WebApi.Features;
+
+public interface IApplication
+{
+    Task<List<BlogDataModel>> GetBlogs();
+    Task<BlogDataModel> GetBlogById(int id);
+    Task<int> CreateBlog(BlogDataModel reqModel);
+    Task<int> PutBlog(int id, BlogDataModel reqModel);
+    Task<int> PatchBlog(int id, BlogDataModel reqModel);
+    Task<int> DeleteBlog(int id);
+}
