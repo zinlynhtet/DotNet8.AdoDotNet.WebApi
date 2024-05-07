ae274e61e0d836b6d96f0847e894cb98069552e8
Zin Linn Htet <mackk5504@gmail.com>
2024-05-07 14:23:57 +0630

    -

--- DotNet8.AdoDotNet.WebApi/GlobalUsings.cs
+++ DotNet8.AdoDotNet.WebApi/GlobalUsings.cs
@@ -1,4 +1,9 @@
 ﻿// Global using directives
 
+global using System.Data.SqlClient;
 global using DotNet8.AdoDotNet.Shared;
+global using DotNet8.AdoDotNet.WebApi.Features;
 global using DotNetTrainingBatch4.RestApi.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
