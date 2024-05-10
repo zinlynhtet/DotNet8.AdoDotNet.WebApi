c4863357f716c483e68b0c1583528815020b33f7
Zin Linn Htet <mackk5504@gmail.com>
2024-05-10 10:10:09 +0630

    Complete Dependency Injection

--- DotNet8.AdoDotNet.WebApi/Program.cs
+++ DotNet8.AdoDotNet.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using DotNet8.AdoDotNet.WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<Application>();
+// builder.Services.AddScoped<Application>();
+builder.Services.AddScoped(n => new AdoDotNetService(SqlConnectionString.sqlConnectionStringBuilder.ConnectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
